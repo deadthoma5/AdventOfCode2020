a0e4e2acb75af6f005bc0cf31463771b0ae31508
Thomas Tran <ttran@epic.com>
2020-12-07 20:43:19 -0600

    Part 1 but slow (2 hours)

---
+++ 7/7.1.py
@@ -0,0 +1,41 @@
+import shared
+
+testing = False
+
+if testing:
+    text = shared.read_input("input_test")
+else:
+    text = shared.read_input("input")
+
+rules = shared.parse_rules(text)
+
+if testing:
+    print(rules)
+
+count_gold = 0
+sum = 0
+
+def get_contents(color):
+    global count_gold
+    if rules[color]:
+        if color == 'shiny gold':
+            count_gold += 1
+        for contents_color, quantity in rules[color].items():
+            for index in range(quantity):
+                get_contents(contents_color)
+    else:
+        return
+    
+    return rules[color]
+
+for color in rules:
+    count_gold = 0
+    get_contents(color)
+    if (color != "shiny gold") and (count_gold > 0):
+        sum += 1
+
+print("\nTotal bag colors that contain at least one shiny gold bag: ", sum)
+
+shared.printTimeElapsed()
+
+# expected answer for part 1 = 326
---
+++ 7/shared.py
@@ -0,0 +1,46 @@
+import re
+from time import time
+
+_startTime = None
+
+def read_input(filename):
+    global _startTime
+    with open(filename) as f:
+        data = [l.rstrip("\n") for l in f.readlines()]
+
+    _startTime = time()
+
+    if len(data) == 1:
+        try:
+            return int(data[0])
+        except:
+            try:
+                return [int(i) for i in data[0].split()]
+            except:
+                return data[0]
+    else:
+        try:
+            return [int(i) for i in data]
+        except:
+            return data
+
+def printTimeElapsed():
+    global _startTime
+    _endTime = time()
+    print("Time: {:.3f}s".format(_endTime - _startTime))
+
+def parse_rules(text):
+    parsed = dict()
+    for line in text:
+        key, value = line.rstrip(".\n").split(" bags contain ")
+        parsed[key] = {}
+        for section in value.split(", "):
+            try:
+                pattern = re.compile("(\d+)( )([a-zA-Z ]+)( bag)([s]?)")
+                match = pattern.match(section).groups()
+                new_contents = dict()
+                new_contents[match[2]] = int(match[0])
+                parsed[key].update(new_contents)
+            except AttributeError:
+                parsed[key] = {}
+    return parsed
