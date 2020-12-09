09ab11163ce654c5fc12b6cbf3f227bcb5e249a3
Thomas Tran <ttran@epic.com>
2020-12-09 14:36:50 -0600

    removed unnecessary outer loop

--- 9/9.2.py
+++ 9/9.2.py
@@ -21,27 +21,21 @@ for i in range(preamble,len(text)):
     if text[i] not in sums:
         part1 = text[i]
         break
-
 print(f"[Part 1] This value doesn't have a match in its preceding preamble: {part1}")
 
 # Part 2
 p1index = text.index(part1)
-for i in range(p1index):
-    flag = False
-    indexcombos = list(combinations(range(p1index),2))
-    ranges = [(start,end+1) for start, end in indexcombos]
-    for start, end in ranges:
-        sums = [sum(text[start:end])]
-        if part1 in sums:
-            if testing:
-                print(f"we got a match at indices: {start} thru {end-1}")
-            res = text[start:end]
-            part2 = min(res) + max(res)
-            flag = True
-            break
-    if flag:
+indexcombos = list(combinations(range(p1index),2))
+ranges = [(start,end+1) for start, end in indexcombos]
+for start, end in ranges:
+    sums = [sum(text[start:end])]
+    if part1 in sums:
+        if testing:
+            print(f"we got a match at indices: {start} thru {end-1}")
+        res = text[start:end]
+        part2 = min(res) + max(res)
+        flag = True
         break
-
 print(f"[Part 2] The sum of min/max values are: {part2}")
 
 shared.printTimeElapsed()
