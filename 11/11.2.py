591e7ea77918370d91ce74d7edcbc18bec6cd057
Thomas Tran <ttran@epic.com>
2020-12-11 20:13:12 -0600

    comment cleanup and efficiency improvements

--- 11/11.2.py
+++ 11/11.2.py
@@ -10,26 +10,18 @@ else:
     seats = shared.read_input("input")
 
 def moveTarget(target, direction):
-    movedTarget = tuple(map(lambda a,b: a+b, target, direction))
-    return movedTarget
+    return tuple(map(lambda a,b: a+b, target, direction))
 
 def isTargetOutOfBounds(target, rowmax, colmax):
     y, x = target
     if (x < 0) or (x >= colmax) or (y < 0) or (y >= rowmax):
-        #if testing:
-        #    print("out of bounds")
         return True
     else:
-        #if testing:
-        #    print("not out of bounds")
         return False
 
-def getTarget(seats, target):
-    y, x = target
-    return seats[y][x]
-
 def evalSeat(seats, row, col, rowmax, colmax):
-    if seats[row][col] == '.':
+    seat = seats[row][col]
+    if seat == '.':
         return '.'
     else:
         count = 0
@@ -39,40 +31,17 @@ def evalSeat(seats, row, col, rowmax, colmax):
         for direction in directions:
             target = (row, col)
             target = moveTarget(target, direction)
-            #if testing:
-            #    print(f"checking target position: {target} | heading: {direction} | seat: {row}, {col}")
             while not isTargetOutOfBounds(target, rowmax, colmax):
-            #    if testing:
-            #        print(f"checking target position: {target} | heading: {direction} | seat: {row}, {col}")
-                targetSeat = getTarget(seats, target)
+                targetSeat = seats[target[0]][target[1]]
                 if targetSeat == '.':
-            #        if testing:
-            #            print(f"seat: {row, col}, target {target} is: ., ")
                     target = moveTarget(target, direction)
                     continue
                 elif targetSeat == '#':
                     count += 1
-            #        if testing:
-            #            print(f"seat: {row, col}, target {target} is: #, ")
                     break
                 elif targetSeat == 'L':
-            #        if testing:
-            #            print(f"seat: {row, col}, target {target} is: L, ")
                     break
-        if seats[row][col] == 'L':
-            if count == 0:
-            #    if testing:
-            #        print(f"Flipping L to #, count: {count}")
-                return '#'
-            else:
-                return 'L'
-        elif seats[row][col] == '#':
-            if count >= 5:
-            #    if testing:
-            #        print(f"Flipping # to L, count: {count}")
-                return 'L'
-            else:
-                return '#'
+        return 'L' if (seat == 'L' and count != 0) or (seat == '#' and count >= 5) else '#'
 
 # Part 2
 rowmax, colmax = len(seats), len(seats[0])
