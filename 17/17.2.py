b289e9a1ae64071ae52901584ab41d20764590e6
Thomas Tran <tom.l.tran@gmail.com>
2020-12-17 02:17:39 -0600

    Day 17 Part 2 but poor optimization (526s on RPi4)

--- 17/17.2.py
+++ 17/17.2.py
@@ -16,24 +16,26 @@ def init_grid(text):
     startx = origin - startingWidth // 2
     starty = startx
     startz = origin
-    grid = np.zeros((dim, dim, dim), dtype=int)
+    startw = origin
+    grid = np.zeros((dim, dim, dim, dim), dtype=int)
     for y in range(starty, starty+startingWidth):
         for x in range(startx, startx+startingWidth):
             if text[y - starty][x - startx] == '#':
-                grid[startz][y][x] = 1
+                grid[startw][startz][y][x] = 1
             else:
-                grid[startz][y][x] = 0
+                grid[startw][startz][y][x] = 0
     return grid
 
-def evaluate(grid, z, y, x):
+def evaluate(grid, w, z, y, x):
     count = 0
-    gridValue = grid[z][y][x]
-    for k in range(z-1, z+2):
-        for j in range(y-1, y+2):
-            for i in range(x-1, x+2):
-                if (k != z) or (j != y) or (i != x):
-                    if grid[k][j][i] == 1:    # neighbor is active
-                        count += 1
+    gridValue = grid[w][z][y][x]
+    for l in range(w-1, w+2):
+        for k in range(z-1, z+2):
+            for j in range(y-1, y+2):
+                for i in range(x-1, x+2):
+                    if (l != w) or (k != z) or (j != y) or (i != x):
+                        if grid[l][k][j][i] == 1:    # neighbor is active
+                            count += 1
     if gridValue == 1:
         if (count == 2) or (count == 3):
             return 1
@@ -48,39 +50,31 @@ def evaluate(grid, z, y, x):
 def count_cubes(grid):
     return np.count_nonzero(grid == 1)
 
-# Part 1
-if testing:
-    cycles = 3
-else:
-    cycles = 6
+# Part 2
+cycles = 6
 startingWidth = len(text[0])
-dim = startingWidth + cycles*2 + 1
+dim = startingWidth + cycles*3
 if testing:
     print(f"dim: {dim}")
 
-# note: z y x (0,0,0 = first and top-left)
+# note: w z y x (0,0,0,0 = first and top-left)
 grid = init_grid(text)
 if testing and printGrids:
     print("Before any cycles:")
-    print("z=0")
-    print(grid[3][:][:])
+    print("w=0, z=0")
+    print(grid[dim // 2][dim // 2][:][:])
 
 for c in range(6):
     work = grid.copy()
-    for z in range(1, dim - 1):
-        for y in range(1, dim - 1):
-            for x in range(1, dim - 1):
-                work[z][y][x] = evaluate(grid, z, y, x)
-    grid = work
-    if testing and printGrids:
-        print(f"\nAfter {c+1} cycles:")
+    for w in range(1, dim - 1):
         for z in range(1, dim - 1):
-            print(f"z={z}")
-            print(grid[z][:][:])
-            print()
+            for y in range(1, dim - 1):
+                for x in range(1, dim - 1):
+                    work[w][z][y][x] = evaluate(grid, w, z, y, x)
+    grid = work
 
-part1 = count_cubes(grid)
-print(f"[Part 1] {part1}")
+part2 = count_cubes(grid)
+print(f"[Part 2] {part2}")
 
 # Display the time this took to run
 shared.printTimeElapsed()
